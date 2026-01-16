985b6bd1198a17a8b02a3b01687dd5e9fb21c87e
Neha Pandey <111239723+ne-ey@users.noreply.github.com>
2026-01-16 20:59:42 +0530

    Time: 943 ms (88.35%) | Memory: 267.1 MB (66.02%) - LeetSync

---
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        const int MOD = 1e9 + 7;
+
+      
+        hFences.push_back(1);
+        hFences.push_back(m);
+        vFences.push_back(1);
+        vFences.push_back(n);
+
+     
+        sort(hFences.begin(), hFences.end());
+        sort(vFences.begin(), vFences.end());
+
+        
+        unordered_set<int> hGaps;
+        for (int i = 0; i < hFences.size(); i++) {
+            for (int j = i + 1; j < hFences.size(); j++) {
+                hGaps.insert(hFences[j] - hFences[i]);
+            }
+        }
+
+        long long maxSide = 0;
+        for (int i = 0; i < vFences.size(); i++) {
+            for (int j = i + 1; j < vFences.size(); j++) {
+                int gap = vFences[j] - vFences[i];
+                if (hGaps.count(gap)) {
+                    maxSide = max(maxSide, (long long)gap);
+                }
+            }
+        }
+
+        if (maxSide == 0) return -1;
+        return (maxSide * maxSide) % MOD;
+    }
+};
